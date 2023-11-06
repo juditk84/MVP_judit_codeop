c813a9f54cc5b1603bbdb3d9d026cb8e3ea86321
Judit Calero <judit.calero84@gmail.com>
2023-11-06 17:12:35 +0100

    just testing connecting trying things

--- model/init_db.sql
+++ model/init_db.sql
@@ -3,16 +3,22 @@
 --
 
 SET foreign_key_checks = 0;
-DROP TABLE if exists students;
+DROP TABLE if exists exercicis;
 SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
 
-CREATE TABLE students(
+CREATE TABLE exercicis(
     id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
+    tipus VARCHAR(40) not null, 
+    subtipus VARCHAR(40) not null,
+    imatge VARCHAR(100) not null,
+    so VARCHAR(100) not null,
+    completat BOOLEAN not null,
     PRIMARY KEY (id)
     );
+
+INSERT INTO exercicis (tipus, subtipus, imatge, so, completat) VALUES ("acord", "triada_major", "./src/assets/images/triada_major.png", "./src/assets/sounds/triada_major.mp3", false)
+INSERT INTO exercicis (tipus, subtipus, imatge, so, completat) VALUES ("acord", "triada_menor", "./src/assets/images/triada_menor.png", "./src/assets/sounds/triada_menor.mp3", false)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({ title: 'Express' });
+  res.send({ title: 'welcome to the API (Abnormal Protruding Intestine)' });
 });
+
+router.get('/api/exercicis', function(req, res, next) {
+  db("SELECT * FROM exercicis;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+router.get('/api/exercicis/:id', function(req, res, next) {
+
+  const result = db(`SELECT * FROM exercicis WHERE id = ${req.params.id};`)
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+
 
 module.exports = router;
